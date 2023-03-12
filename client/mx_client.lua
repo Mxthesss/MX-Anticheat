5148304bc2713ec2e101f53d3bebd7407069bff0
Matěj Bednář <99074840+Mxthesss@users.noreply.github.com>
2023-03-13 00:19:15 +0100

    Update 1.0.1

--- client/mx_client.lua
+++ client/mx_client.lua
@@ -15,13 +15,13 @@ Citizen.CreateThread(function()
 			nothing, weapon = GetCurrentPedWeapon(playerPed, true)
 			if isWeaponBlacklisted(weapon) then 
 		  RemoveWeaponFromPed(playerPed, weapon)
-		 --[[   exports['nui']:requestScreenshotUpload("https://discord.com/api/webhooks/819901517851131914/eoX5MGBtjknyOq387oN96xu2gtoq6sofu4Cqpei9nTYLOoYYhGWd78nKGUhURPA8gVu_", "files[]", function(data)end)
-			TriggerServerEvent("notbad:log","Give Weapon")]]
+		  TriggerServerEvent("mx_HWIDBAN", sender, "Black Listed Weapon")
 			end
 		end
 	end
 end)
 end
+
 function isWeaponBlacklisted(model)
 	for _, blacklistedWeapon in pairs(unkw.zbrane) do
 		if model == GetHashKey(blacklistedWeapon) then
